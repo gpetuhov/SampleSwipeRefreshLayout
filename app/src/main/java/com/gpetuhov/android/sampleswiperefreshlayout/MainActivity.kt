28c1ede82f7a9454ee5163f2a8bc961df3e711db
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-10-02 12:10:19 +0300

    Add SwipeRefreshLayout and menu

--- app/src/main/java/com/gpetuhov/android/sampleswiperefreshlayout/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/sampleswiperefreshlayout/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.gpetuhov.android.sampleswiperefreshlayout
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +9,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    // Inflate menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
     }
 }
